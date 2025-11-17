feb8f3d77e3a070e748600dcb2fe4e01884c8551
Drenn <kraananas@gmail.com>
2025-11-16 19:12:09 -0500

    Added some things

--- src/main/java/com/drenn/cartographica/client/ClientEvents.java
+++ src/main/java/com/drenn/cartographica/client/ClientEvents.java
@@ -5,19 +5,19 @@ import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 import java.security.Key;
-
+@EventBusSubscriber(modid = Cartographica.MOD_ID, value = Dist.CLIENT)
 public class ClientEvents {
-    public static void onPlayerTick(PlayerTickEvent.Post event) {
-        if (!event.getEntity().level().isClientSide()){
-            return;
-        }
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
         Minecraft mc = Minecraft.getInstance();
-        if (mc.player == null || mc.player != event.getEntity()) {
+        if (mc.player == null) {
             return;
         }
+
         if (KeyBindings.OPEN_MAP.consumeClick()) {
             Cartographica.LOGGER.info("M key was pressed!");
             mc.player.sendSystemMessage(
